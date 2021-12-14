6418d903d2d3192997d106c608068250ab14cb10
cunyu1943 <747731461@qq.com>
2021-12-14 09:19:04 +0800

    🎉 update mybatis-generator-demo

--- mybatis-generator-demo/src/main/java/com/cunyu1943/mybatisgeneratordemo/entity/User.java
+++ mybatis-generator-demo/src/main/java/com/cunyu1943/mybatisgeneratordemo/entity/User.java
@@ -1,8 +1,9 @@
 package com.cunyu1943.mybatisgeneratordemo.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+public class User implements Serializable {
     private Integer id;
 
     private String username;
@@ -22,6 +23,8 @@ public class User {
     private String headUrl;
 
     private Date createTime;
+
+    private static final long serialVersionUID = 1L;
 
     public Integer getId() {
         return id;
@@ -101,5 +104,66 @@ public class User {
 
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        User other = (User) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+            && (this.getSalt() == null ? other.getSalt() == null : this.getSalt().equals(other.getSalt()))
+            && (this.getEmail() == null ? other.getEmail() == null : this.getEmail().equals(other.getEmail()))
+            && (this.getType() == null ? other.getType() == null : this.getType().equals(other.getType()))
+            && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
+            && (this.getActivationCode() == null ? other.getActivationCode() == null : this.getActivationCode().equals(other.getActivationCode()))
+            && (this.getHeadUrl() == null ? other.getHeadUrl() == null : this.getHeadUrl().equals(other.getHeadUrl()))
+            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getSalt() == null) ? 0 : getSalt().hashCode());
+        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
+        result = prime * result + ((getType() == null) ? 0 : getType().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
+        result = prime * result + ((getActivationCode() == null) ? 0 : getActivationCode().hashCode());
+        result = prime * result + ((getHeadUrl() == null) ? 0 : getHeadUrl().hashCode());
+        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", salt=").append(salt);
+        sb.append(", email=").append(email);
+        sb.append(", type=").append(type);
+        sb.append(", status=").append(status);
+        sb.append(", activationCode=").append(activationCode);
+        sb.append(", headUrl=").append(headUrl);
+        sb.append(", createTime=").append(createTime);
+        sb.append("]");
+        return sb.toString();
     }
 }
--- mybatis-generator-demo/src/main/java/com/cunyu1943/mybatisgeneratordemo/mapper/UserMapper.java
+++ mybatis-generator-demo/src/main/java/com/cunyu1943/mybatisgeneratordemo/mapper/UserMapper.java
@@ -1,17 +1,16 @@
 package com.cunyu1943.mybatisgeneratordemo.mapper;
 
 import com.cunyu1943.mybatisgeneratordemo.entity.User;
+import java.util.List;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
 
     int insert(User record);
 
-    int insertSelective(User record);
-
     User selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(User record);
+    List<User> selectAll();
 
     int updateByPrimaryKey(User record);
 }
